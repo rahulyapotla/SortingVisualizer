f3182ab67f7720af1beeb08857206e9b822bf403
rahulyapotla <prahulya@gmail.com>
2024-02-20 16:32:24 -0500

    Added merge sort visualizer

---
+++ visualizer/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <SortingVisualizer/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
